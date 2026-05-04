554376f31556dd3170ed84ae9d40b6803560cf4a
Sahana052003 <sahananreddy52003@gmail.com>
2026-05-04 10:03:24 +0530

    used 1.Validations and 2. DTO
    HttpServlet,doPOST(),getParameter,setContentType,PrintWriter,value

--- ownly/src/com/xworkz/ownly/main/Ownly.java
+++ ownly/src/com/xworkz/ownly/main/Ownly.java
@@ -1,11 +1,17 @@
 package com.xworkz.ownly.main;
 
+import com.xworkz.ownly.dto.OwnlyDTO;
+import com.xworkz.ownly.service.OwnlyValidation;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/run",loadOnStartup = 2)
 public class Ownly extends HttpServlet {
@@ -14,7 +20,40 @@ public class Ownly extends HttpServlet {
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        super.service(req, res);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String name = req.getParameter("name");
+        System.out.println(name);
+        String email = req.getParameter("email");
+        System.out.println(email);
+        String mobileNumber = req.getParameter("mobileNumber");
+        System.out.println(mobileNumber);
+        String age = req.getParameter("age");
+        System.out.println(age);
+        OwnlyDTO ownlyDTO=new OwnlyDTO(name,email,mobileNumber,Integer.parseInt(age));
+        OwnlyValidation ownlyValidation=new OwnlyValidation();
+        boolean validate = ownlyValidation.validate(ownlyDTO);
+        PrintWriter writer = resp.getWriter();
+        if (validate){
+           writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<title>Ownly food delivery </title>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h1>Hi</h1>" + name + age );
+            writer.println("<p style='color:green'> Food is Ordered </p>");
+            writer.println("</body>");
+            writer.println("</html>");
+        }else {
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<title>Ownly food delivery");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h1>Hello</h1>"+ name + age );
+            writer.println("<p style='color:red'> Food is Not Ordered </p>");
+            writer.println("</body>");
+            writer.println("</html>");
+        }
     }
 }
