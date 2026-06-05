faaacbd19c88e7090b5cc61ab33f8d241623de76
Sahana052003 <sahananreddy52003@gmail.com>
2026-06-05 10:00:00 +0530

    Use SendRedirect()

--- forms/src/com/xworkz/forms/formspage/Facebook.java
+++ forms/src/com/xworkz/forms/formspage/Facebook.java
@@ -8,31 +8,32 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
-@WebServlet(urlPatterns = "/login",loadOnStartup = 1)
+@WebServlet(urlPatterns = "/login", loadOnStartup = 1)
 public class Facebook extends GenericServlet {
 
-    public Facebook(){
+    public Facebook() {
         System.out.println("Creating account to login");
     }
+
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        String username=servletRequest.getParameter("username");
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse)
+            throws ServletException, IOException {
+        String username = servletRequest.getParameter("username");
         System.out.println("user name : " + username);
         String name = servletRequest.getParameter("email");
         System.out.println(name);
         String pwd = servletRequest.getParameter("password");
         System.out.println(pwd);
-       // System.out.println("Login successfully");
-        PrintWriter printWriter=servletResponse.getWriter();
-        printWriter.println(username+"Login Successfully");
+        // System.out.println("Login successfully");
+        PrintWriter printWriter = servletResponse.getWriter();
+        printWriter.println(username + "Login Successfully");
         servletResponse.setContentType("text/html/plain");
         printWriter.println("<html>");
         printWriter.println("<head>");
         printWriter.println("<title>Login Page</title>");
         printWriter.println("</head>");
         printWriter.println("<body>");
-        printWriter.println("<h1>HI,"+  username + "</h1>");
+        printWriter.println("<h1>HI," + username + "</h1>");
         printWriter.println("<p>Successfully registered</p>");
         printWriter.println("</body></html>");
     }
